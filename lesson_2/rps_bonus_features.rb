d874ae2a5eae44af2f4cbd4a4e87018099b4f5c9
Sam Norton <samuelnico.norton@gmail.com>
2016-09-15 15:15:54 +0800

    My final update for my rps_bonus_features.rb

--- lesson_2/rps_bonus_features.rb
+++ lesson_2/rps_bonus_features.rb
@@ -9,11 +9,11 @@ VALID_CHOICES = {
 }
 
 WINNING_COMBO = {
-  'r' => ['sc', 'l'],
-  'p' => ['r', 'sp'],
-  'sc' => ['p', 'l'],
-  'l' => ['sp', 'p'],
-  'sp' => ['sc', 'r']
+  'r' => %w(sc l),
+  'p' => %w(r sp),
+  'sc' => %w(p l),
+  'l' => %w(sp p),
+  'sp' => %w(sc r)
 }
 
 def prompt(message)
@@ -25,68 +25,61 @@ def win?(first, second)
 end
 
 def clear_screen
- system('clear') || system('cls')
+  system('clear') || system('cls')
 end
 
-counts = {player: 0, computer: 0}
-
+counts = { player: 0, computer: 0 }
 
 def display_results(player, computer, counts)
   prompt(
-    case
-    when counts[:player] == 5 then "MESSAGE: You won the game! Congrats!"
-    when counts[:computer] == 5 then "MESSAGE:  Computer won the game. Try again?"
-    when win?(player, computer) then "MESSAGE:  You won this round!"
-    when win?(computer, player) then "MESSAGE:  Computer won this round! You loose!"
-    else "MESSAGE:  It's a tie on this round! No one wins!"
+    if counts[:player] == 5 then "MSG: You won this game! (5 wins)!"
+    elsif counts[:computer] == 5 then "MSG: Computer won this game! (5 wins)"
+    elsif win?(player, computer) then "MSG: You won this round!"
+    elsif win?(computer, player) then "MSG: Computer won this round! You loose!"
+    else "MSG: It's a tie on this round! No one wins!"
     end
   )
 end
 
 def count_wins(player, computer, counts)
-    if win?(player, computer)
-      counts[:player] += 1
-    elsif win?(computer, player)
-      counts[:computer] += 1
-    end
-    prompt("Player Count: #{counts[:player]}")
-    prompt("Computer Count: #{counts[:computer]}")
+  if win?(player, computer)
+    counts[:player] += 1
+  elsif win?(computer, player)
+    counts[:computer] += 1
+  end
+  prompt("Player Count: #{counts[:player]}")
+  prompt("Computer Count: #{counts[:computer]}")
 end
 
-
-  loop do # main loop
-    choice = ''
-    loop do
-      prompt("Choose a key:")
-      VALID_CHOICES.each do |short_name, complete_name|
-        prompt("'#{short_name}' for #{complete_name}")
-      end
-      choice = Kernel.gets().chomp().downcase()
-
-      if VALID_CHOICES.include?(choice)
-        break
-      else
-        prompt("That's not a valid choice.")
-      end
+loop do # main loop
+  choice = ''
+  loop do
+    prompt("Choose a key:")
+    VALID_CHOICES.each do |short_name, complete_name|
+      prompt("'#{short_name}' for #{complete_name}")
     end
+    choice = Kernel.gets().chomp().downcase()
 
-    computer_choice = VALID_CHOICES.keys.sample
-    prompt("You chose: #{VALID_CHOICES[choice]}")
-    prompt("Computer choose: #{VALID_CHOICES[computer_choice]}")
-
-
-    if counts[:player] > 5 || counts[:computer] > 5
-      counts = {player: 0, computer: 0}
+    if VALID_CHOICES.include?(choice)
       break
     else
-    count_wins(choice, computer_choice, counts)
+      prompt("That's not a valid choice.")
     end
+  end
 
-    display_results(choice, computer_choice, counts)
-    prompt("Do you want to play again? ('yes' or 'no')")
-    
+  computer_choice = VALID_CHOICES.keys.sample
+  prompt("You chose: #{VALID_CHOICES[choice]}")
+  prompt("Computer choose: #{VALID_CHOICES[computer_choice]}")
 
-  loop do  
+  if counts[:player] >= 5 || counts[:computer] >= 5
+    counts = { player: 0, computer: 0 }
+  end
+
+  count_wins(choice, computer_choice, counts)
+  display_results(choice, computer_choice, counts)
+  prompt("Do you want to play again? ('yes' or 'no')")
+
+  loop do
     answer = Kernel.gets().chomp()
     if answer.casecmp('yes').zero?
       break
@@ -97,7 +90,6 @@ end
       prompt("Please enter either 'yes' or 'no'.")
     end
   end
-    clear_screen
-  end
-    
-
+  clear_screen
+end
+prompt("Thank you for playing!")
